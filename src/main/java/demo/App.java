543c390fc8c451f018dd240a5df2ed77cefb40c8
jlnaviaa <joseluisnavia@outlook.com>
2021-08-18 22:43:43 -0500

    Solucion Ciclo 2 Reto 4

---
+++ src/main/java/demo/App.java
@@ -0,0 +1,24 @@
+package demo;
+
+import demo.view.*;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        VistaRequerimientosReto4.requerimiento1();
+
+        System.out.println("-----------");
+
+        VistaRequerimientosReto4.requerimiento2();
+
+        System.out.println("-----------");
+
+        VistaRequerimientosReto4.requerimiento3();
+
+        System.out.println("---Felicitaciones lo ha logrado---");
+
+
+
+    }
+}
